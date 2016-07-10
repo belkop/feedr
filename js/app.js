0797d157b6d3b264f9212bdb3186a745d66087bf
belkop <belkopcheff@Bels-MacBook-Pro.local>
2016-07-10 15:26:06 +1000

    update2

--- js/app.js
+++ js/app.js
@@ -6,8 +6,15 @@ $.get("https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/new
   results.data.feed.forEach(function(result){
     var source   = $("#entry-template").html();
     var template = Handlebars.compile(source);
-    var context = {title: result.content.title, body: "This is my first post!"};
-    var html    = template(context);
+    var context = {
+      title: result.content.title,
+      body: "This is my first post!",
+      image: result.content.media.images[0].url,
+      shares:result.fb_shares.count
+    };
+    var html = template(context);
+      console.log(result.fb_shares)
+
 
     // $("#main").append("<li>"+result.content.title+"</li>")
     $("#main").append(html)
